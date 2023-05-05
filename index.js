4229a3f9a347faf726051ee1f9299e61c8ba9ad0
Ismail K <ismailkoormath037@gmail.com>
2023-05-05 18:56:48 +0530

    userModel created

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const dbConnect = require('./config/dbConnection')
 const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 
+// database connection function
+dbConnect()
+
+app.use("/",(req,res)=>{
+    res.send('Hello from server side')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at PORT ${PORT}`);
+})
