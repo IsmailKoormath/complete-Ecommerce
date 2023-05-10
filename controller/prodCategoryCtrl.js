e902d5bade0aa1eee9885b417ec819d46a277a3a
Ismail K <ismailkoormath037@gmail.com>
2023-05-10 19:35:47 +0530

    update product category

--- controller/prodCategoryCtrl.js
+++ controller/prodCategoryCtrl.js
@@ -1,5 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const ProdCategory = require("../models/prodCategoryModel");
+const { trusted } = require("mongoose");
+
+// create product category
+
 const createprodCategory = asyncHandler(async (req, res) => {
   try {
     const addProdCategoty = await ProdCategory.create(req.body);
@@ -9,4 +13,18 @@ const createprodCategory = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createprodCategory };
+// update product category
+
+const updateprodCategory = asyncHandler(async (req, res) => {
+    const {id}= req.params
+  try {
+    const updateCategoty = await ProdCategory.findByIdAndUpdate(id,req.body,{
+        new:trusted
+    })
+    res.json(updateCategoty);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {createprodCategory, updateprodCategory };
--- routes/ProdCategoryRouter.js
+++ routes/ProdCategoryRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createprodCategory } = require("../controller/prodCategoryCtrl");
+const { createprodCategory, updateprodCategory } = require("../controller/prodCategoryCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 
 router.post('/',authMiddleware,isAdmin,createprodCategory)
+router.put('/:id',authMiddleware,isAdmin,updateprodCategory)
 
 module.exports = router
