0eea96cae7bbb3595926040829996daeac2f15b3
Ismail K <ismailkoormath037@gmail.com>
2023-05-11 18:24:22 +0530

    comment added on the ratings section

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -143,8 +143,7 @@ const wishlist = asyncHandler(async (req, res) => {
 
 const rating = asyncHandler(async (req, res) => {
   const { _id } = req.user;
-  console.log(_id);
-  const { star, prodId } = req.body;
+  const { star, prodId,comment } = req.body;
   try {
     const product = await Product.findById(prodId);
     let alreadyRated = product.ratings.find(
@@ -156,7 +155,7 @@ const rating = asyncHandler(async (req, res) => {
           ratings: { $elemMatch: alreadyRated },
         },
         {
-          $set: { "ratings.$.star": star },
+          $set: { "ratings.$.star": star, "ratings.$.comment":comment },
         },
         {
           new: true,
@@ -169,6 +168,7 @@ const rating = asyncHandler(async (req, res) => {
           $push: {
             ratings: {
               star: star,
+              comment:comment,
               postedby: _id,
             },
           },
