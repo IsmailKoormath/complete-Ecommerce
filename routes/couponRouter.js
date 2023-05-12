b80006c9a67f17edf148fc7c01dec8a5681bf7e4
Ismail K <ismailkoormath037@gmail.com>
2023-05-12 19:52:48 +0530

    check the admin can only create coupon and validate and token

--- routes/couponRouter.js
+++ routes/couponRouter.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { createCoupon } = require('../controller/couponCtrl')
+const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
 const router = express.Router()
 
-router.post("/",createCoupon)
+router.post("/",authMiddleware,isAdmin,createCoupon)
 
 module.exports = router
