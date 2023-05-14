c95e98812d9a01b78d10e87178753297f529d26c
Ismail K <ismailkoormath037@gmail.com>
2023-05-14 21:40:57 +0530

    update coupon controller created

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -1,22 +1,34 @@
 const asyncHandler = require("express-async-handler");
-const Coupon = require('../models/couponModel')
+const Coupon = require("../models/couponModel");
 
-const createCoupon = asyncHandler(async(req,res)=>{
-try {
-    const newCoupon = await Coupon.create(req.body)
-    res.json(newCoupon)
-} catch (error) {
+const createCoupon = asyncHandler(async (req, res) => {
+  try {
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getallCoupon = asyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatecoupon = await Coupon.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatecoupon)
+  } catch (error) {
     throw new Error(error)
-}
-})
+  }
+});
 
-const getallCoupon = asyncHandler(async(req,res)=>{
-try {
-    const coupons = await Coupon.find()
-    res.json(coupons)
-} catch (error) {
-    throw new Error(error)
-}
-})
-
-module.exports = {createCoupon,getallCoupon}
+module.exports = { createCoupon, getallCoupon, updateCoupon };
